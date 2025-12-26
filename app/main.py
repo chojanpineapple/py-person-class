1eb76087dd4df6ef4054b33589c3df51e3ec9afb
Johan Rangel <johan.apr99@gmail.com>
2025-12-26 17:26:17 +0000

    test person1

--- app/main.py
+++ app/main.py
@@ -4,10 +4,26 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+        self.people[name] = Person
 
 
 def create_person_list(people: list) -> list:
+    result = [
+    (
+        {**p, "wife": p.get("wife") or next(
+            (x["name"] for x in people if x.get("husband") == p["name"]),
+            None
+        )}
+        if "wife" in p
+        else
+        {**p, "husband": p.get("husband") or next(
+            (x["name"] for x in people if x.get("wife") == p["name"]),
+            None
+        )}
+    )
+    for p in people
+]
+
     for p in people:
         person = Person(p["name"], p["age"])
-        person.people[p["name"]] = person
-    return person.__dict__
+    return result
