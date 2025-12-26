8a04df6b477ca3f0b2a5a4b9a839c1a5f3d394e6
Johan Rangel <johan.apr99@gmail.com>
2025-12-26 14:48:48 +0000

    test person

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for p in people:
+        person = Person(p["name"], p["age"])
+        person.people[p["name"]] = person
+    return person.__dict__
